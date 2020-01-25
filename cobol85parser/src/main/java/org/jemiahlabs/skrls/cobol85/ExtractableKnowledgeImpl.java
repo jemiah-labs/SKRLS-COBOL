218af2f3fb632090aacab00f6d2dff7ee9b17869
Alan M.E <alanmarquez@outlook.com>
2020-01-25 16:35:25 -0500

    capture runtime exception

--- cobol85parser/src/main/java/org/jemiahlabs/skrls/cobol85/ExtractableKnowledgeImpl.java
+++ cobol85parser/src/main/java/org/jemiahlabs/skrls/cobol85/ExtractableKnowledgeImpl.java
@@ -63,6 +63,8 @@ public class ExtractableKnowledgeImpl implements ExtractableKnowledge {
 				
 			} catch (IOException e) {
 				producer.emitWarningMessage(e.getMessage());
+			} catch (RuntimeException ex) {
+				producer.emitErrorMessage(ex.getMessage());
 			}
 		});
 		
